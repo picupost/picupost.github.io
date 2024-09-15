c146fdc1e8af20a14bdcbcb8ef1036c1e6a4411b
picupost <picupostcontacto@gmail.com>
2024-09-15 18:30:29 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const galleryItems = document.querySelectorAll('.gallery-item');
 
-    // Efecto de aparición cuando se hace scroll
     const observer = new IntersectionObserver(entries => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
@@ -17,7 +16,6 @@ document.addEventListener('DOMContentLoaded', () => {
         observer.observe(item);
     });
 
-    // Efecto de zoom al pasar el ratón
     galleryItems.forEach(item => {
         item.addEventListener('mouseenter', () => {
             item.querySelector('img').classList.add('zoom');
@@ -28,3 +26,4 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
